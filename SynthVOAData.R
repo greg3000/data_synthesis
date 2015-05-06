e5a7a74804f4d6d96e746df96881fd66abb166b1
decc <decc@decc-PORTEGE-Z830.(none)>
2015-05-06 09:35:41 +0100

    fixed predictor matrix to use all vars as predictors and added data export code

--- SynthVOAData.R
+++ SynthVOAData.R
@@ -14,10 +14,32 @@ dataV <- data[ which(data$Gcons2012Valid=='V'
 #set a random seed to allow recreatable results
 my.seed=16486374
 
-#select order and which variables to be synthesized
-visit.sequence <- c(51, 52, 53, 54, 55, 56, 57, 60)
+#select order of vars to be synthesized
+visit.sequence <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60) 
+#select method of vars to be synthesized NB "" means no method i.e. this is not synthesized
 method.ini <- c("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "ctree", "ctree", "ctree", "ctree", "ctree", "ctree", "ctree", "", "", "ctree")
+#first run at synthesis - this is mainly done to create a predictor matrix to be edited
+#below ready for final synthesis run
 sds.selection <-syn(dataV, visit.sequence, seed = my.seed, method = method.ini, drop.net.used = FALSE)
 
-#nest step is to have a look at what the above steop creates,
-#and see page 16 of documentation for comparrison code
+
+#edit the predictor matrix to use all vars to predict VOA vars (sets all values in matrix to 1)
+sds.selection$predictor.matrix[sds.selection$predictor.matrix < 1] <- 1
+
+sds.selection <-syn(dataV, visit.sequence, seed = my.seed, method = method.ini, drop.net.used = FALSE)
+
+#see page 16 of documentation for comparrison code
+#do i need to state that data are categorical?
+
+compare.synds(sds.selection$syn, dataV)
+summary(sds.selection)
+
+#summarise the synthesied data
+summary(sds.selection$syn)
+
+#export the summarised data
+synthVOA <- as.data.frame(sds.selection$syn)
+write.csv(synthVOA, file = "synthVOA.csv")
+write.csv(sds.selection$syn, file = "synthVOAtemp.csv")
+
+#add in a smaller dataset to check the output has worked (something that can be opened in excel)
